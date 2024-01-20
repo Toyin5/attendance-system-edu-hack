53ee8e28c0aec291182de0ae40ee8d83fb9c6d52
ToyinM <toyinmuhammed50@gmail.com>
2024-01-20 17:51:32 +0100

    added auth layout

--- apps/class-app/src/pages/Login.tsx
+++ apps/class-app/src/pages/Login.tsx
@@ -1,9 +1,64 @@
+import { useState } from "react";
 import Layout from "../layouts/Layout";
 
-const Login = () => {
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("Login clicked");
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // Add your login logic here
+  };
   return (
     <Layout>
-      <div>Login</div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
+          <h2 className="text-2xl font-bold mb-4">Class APP Login</h2>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              type="email"
+              id="email"
+              placeholder="john.doe@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              type="password"
+              id="password"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              onClick={handleLogin}
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
--- apps/class-app/src/pages/Register.tsx
+++ apps/class-app/src/pages/Register.tsx
@@ -1,9 +1,64 @@
+import { useState } from "react";
 import Layout from "../layouts/Layout";
 
-const Register = () => {
+const Register: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("Register clicked");
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // Add your Register logic here
+  };
   return (
     <Layout>
-      <div>Register</div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
+          <h2 className="text-2xl font-bold mb-4">Class APP Register</h2>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              type="email"
+              id="email"
+              placeholder="john.doe@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              type="password"
+              id="password"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              onClick={handleLogin}
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Register
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
